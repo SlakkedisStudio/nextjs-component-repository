f3ffd15a839ed0380582dc11dad87f631a2e5615
BracketMan <mlourens.erasmus@gmail.com>
2022-11-30 16:43:37 +0200

    Added list of components

---
+++ utils/component-list.ts
@@ -0,0 +1,7 @@
+// import Button from '@/components/component-repository/button';
+// import NavBar from '@/components/component-repository/navbar';
+
+// export const ComponentList = {
+//   button: Button,
+//   navbar: NavBar
+// };
--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,5 @@
-
 import { RegEx } from '../types/regEx';
 
-export const regEx : RegEx = {
-    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+export const regEx: RegEx = {
+  email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 };
